8f4b2b1500d170766c4c9a1775dfc3f52c1de519
Jacqueline Zapanta <jzapanta3@nait.ca>
2022-12-22 20:08:08 -0700

    coding exercise completed

---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,17 @@
+public class MegaBytesConverter {
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes (int kiloBytes) {
+        if (kiloBytes < 0 ) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println( kiloBytes + " KB = "
+                    + kiloBytes / 1024 + " MB and " + kiloBytes % 1024 + " KB");
+        }
+    }
+}
