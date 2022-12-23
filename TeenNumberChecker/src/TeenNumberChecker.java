21bffdb4f8be6ccf86fbb8a3351d79a27d6947b1
Jacqueline Zapanta <jzapanta3@nait.ca>
2022-12-23 00:40:04 -0700

    amended exercise

--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -6,8 +6,10 @@ public class TeenNumberChecker {
     }
 
     public static boolean hasTeen(int num1, int num2, int num3) {
-        return (num1 >= 13 && num1 <= 19)
-                || (num2 >= 13 && num2 <= 19)
-                || (num3 >= 13 && num3 <= 19);
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
+    }
+
+    public static boolean isTeen(int age){
+        return age >= 13 && age <= 19;
     }
 }
