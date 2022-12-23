5bb90d64f6cfcc89296368d4b788b91dbd408e66
Jacqueline Zapanta <jzapanta3@nait.ca>
2022-12-22 20:39:22 -0700

    completed exercise

---
+++ BarkingDog/src/BarkingDog.java
@@ -0,0 +1,22 @@
+public class BarkingDog {
+
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+        System.out.println(shouldWakeUp(true, 44));
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        boolean isDogbarking = false;
+        if (barking)
+        {
+            if  ((hourOfDay > -1 && hourOfDay < 8) || (hourOfDay > 22 && hourOfDay < 24) )  {
+                isDogbarking = true;
+            }
+        }
+        return isDogbarking;
+    }
+}
