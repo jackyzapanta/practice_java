f27e2c3dce815a7a645d1ac5b013bf3179d04464
Jacqueline Zapanta <jzapanta3@nait.ca>
2022-12-24 13:32:11 -0700

    completed exercise

---
+++ Convert2Centimeters/src/Convert2Centimeters.java
@@ -0,0 +1,17 @@
+public class Convert2Centimeters {
+
+    public static void main(String[] args) {
+        System.out.println("First call: " + convertToCentimeters(68) + " cm");
+        System.out.println("Second call: " + convertToCentimeters(5,8) + " cm");
+    }
+
+    public static  double convertToCentimeters(int valueInches) {
+        return (double) valueInches * 2.54;
+    }
+
+    public  static  double convertToCentimeters(int valueFeet, int valueInches) {
+        double convertedValue = (double) (valueFeet * 12) + valueInches;
+        return convertToCentimeters((int)convertedValue);
+    }
+
+}
