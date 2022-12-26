46a7233be80c709e8ac1a3d75bd5cb68aa3cdb62
Jacqueline Zapanta <jzapanta3@nait.ca>
2022-12-26 15:42:01 -0700

    completed exercise

---
+++ Minutes2yearsandDaysCalculator/src/Minutes2yearsandDaysCalculator.java
@@ -0,0 +1,18 @@
+public class Minutes2yearsandDaysCalculator {
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public  static  void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        }
+        else {
+            System.out.println(minutes + " min = "
+            + (minutes / 525600) + " y and " + (minutes % 525600) / 1440  + " d" );
+        }
+    }
+}
